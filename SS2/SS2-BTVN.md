25d0b4b3c294b1e5e16a9f0d7583287f8c2c6933
hieu1966hn <hieu1966hn@gmail.com>
2024-02-02 20:43:30 +0700

    Done kế thừa

---
+++ SS2/SS2-BTVN.md
@@ -0,0 +1,19 @@
+Chưa làm bài: 
+1. Quân: Nhiều bài trên lớp quá.
+2. Khánh: Chưa sx được thời gian (Bài ACE trên lớp)
+3. Tùng Lâm: Lười(chưa đọc đề)
+4. Minh Triết: Chưa sx được thời gian
+
+
+Đã làm bài
+1. Thế Vũ
+2. Đông Du
+
+
+
+
+Lập trình hướng đối tượng
+- Phương pháp lập trình trong đó các đối tượng được tạo ra để lập trình các chức năng cụ thể
+- Tái sử dụng, Bảo trì, Chia nhỏ các module dễ dàng quản lý hơn.
+
+Các dạng kiểu dữ liệu: String, Object, Number, null, Boolean,....
---
+++ SS2/app.js
@@ -0,0 +1,84 @@
+//// Khởi tạo kiểu dữ liệu (đối tượng/lớp)
+class Nguoi {
+  // Hàm khởi tạo thuộc tính cho class này
+  constructor(name, age, gender, country, id, hobbies, job) {
+    // Gán giá trị cho thuộc tính của classs
+    this._name = name;
+    this._age = age;
+    this._gender = gender;
+    this._country = country;
+    this._id = id;
+    this._hobbies = hobbies;
+    this._job = job;
+  }
+
+  cry() {
+    alert("I am crying");
+  }
+
+  gaming() {
+    alert("I am a pro player");
+  }
+
+  introduct() {
+    return `My name is ${this._name} & i am a ${this._job} for 2 years`;
+  }
+}
+
+// // Khai báo biến có kiểu dữ liệu là Nguoi.
+// let TrungHieu = new Nguoi("Nguyễn Trung Hiếu", 26, "Male", "VN",
+//  "00123456789", ["Dev", "Sleeping", "eating"], "BackEnd Developer");
+
+// // /// In ra tên của đối tượng (KDL) vừa khai báo
+// console.log(TrungHieu._name); ///
+
+// // Gọi phương thức
+// TrungHieu.introduct();
+
+////// Tính kế thừa của lập trình hướng đối tượng OOP
+class HocSinh extends Nguoi {
+  constructor(
+    name,
+    age,
+    gender,
+    country,
+    id,
+    hobbies,
+    job,
+    school,
+    classId,
+    studentId
+  ) {
+    // từ khóa Super() giúp kế thừa hết các thuộc tính từ cha.
+    super(name, age, gender, country, id, hobbies, job);
+    this._school = school;
+    this._classId = classId;
+    this._studentId = studentId;
+  }
+
+  // kế thừa hoàn toàn mà không thay đổi gì (cái mình vừa gọi ra y hệt người cha)
+
+  /// Kế thừa và ghi đè hoặc phát triển tiếp từ người cha
+  introduct() {
+    return (
+      super.introduct() +
+      `. And now I am learning at ${this._school} với mã lớp học
+        là ${this._classId}`
+    );
+  }
+}
+
+let TrungHieu = new HocSinh(
+  "Nguyễn Trung Hiếu",
+  26,
+  "Male",
+  "VN",
+  "00123456789",
+  ["Dev", "Sleeping", "eating"],
+  "Student",
+  "MindX School",
+  "NPS-JSI18-HB",
+  "hieunt"
+);
+console.log(TrungHieu._name);
+console.log(TrungHieu.introduct());
