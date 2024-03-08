f8e7f0e3560625e9e4812974295539cfa271ce41
hieu1966hn <hieu1966hn@gmail.com>
2024-03-08 21:07:27 +0700

    New code SS6 done connect Firebase

---
+++ SS4/js/firebase.js
@@ -0,0 +1,19 @@
+ // Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyC1x4GcFNc-BfGwdVsWmMETgGWZ6r5xEBQ",
+   authDomain: "nps-jsi18-hb1.firebaseapp.com",
+   projectId: "nps-jsi18-hb1",
+   storageBucket: "nps-jsi18-hb1.appspot.com",
+   messagingSenderId: "99757781585",
+   appId: "1:99757781585:web:cb07bfcee927877a8dbe70"
+ };
+
+ // Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
--- SS4/js/register.js
+++ SS4/js/register.js
@@ -1,21 +1,30 @@
+// Import các hàm tới từ firebase để lập trình tính năng xác thực người dùng
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+const auth = getAuth(app);
+
 const registerForm = document.getElementById("register-form");
 
 // Bắt sự kiện khi người dùng submit form
 registerForm.addEventListener("submit", (event) => {
-    // Ngăn sự kiện reload mặc định của trình duyệt.
-    event.preventDefault();
+  // Ngăn sự kiện reload mặc định của trình duyệt.
+  event.preventDefault();
 
-    const dataSignUp = {
-        firstName: registerForm.firstName.value.trim(),
-        lastName: registerForm.lastName.value.trim(),
-        email: registerForm.email.value.trim(),
-        password: registerForm.password.value,
-        confirmPassword: registerForm.confirmPassword.value,
-    }
+  const dataSignUp = {
+    firstName: registerForm.firstName.value.trim(),
+    lastName: registerForm.lastName.value.trim(),
+    email: registerForm.email.value.trim(),
+    password: registerForm.password.value,
+    confirmPassword: registerForm.confirmPassword.value,
+  };
 
-    console.log(dataSignUp);
-    // gọi hàm soát lỗi đăng ký người dùng
-    
-    // Trả về dữ liệu người dùng đã được lọc và gán lại vào biến dataSignUpClean
-    let dataSignUpClean = controller.register(dataSignUp);
+  console.log(dataSignUp);
+  // gọi hàm soát lỗi đăng ký người dùng
+
+  // Trả về dữ liệu người dùng đã được lọc và gán lại vào biến dataSignUpClean
+  let dataSignUpClean = controller.register(dataSignUp);
 });
